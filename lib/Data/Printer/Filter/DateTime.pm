e31bd650f2b451a942e3e8cc5d99d946612a03cc
Ivan Bessarabov <ivan@bessarabov.ru>
2012-12-05 17:24:08 +0400

    Added support for Class::Date in D:P:F:DateTime

--- lib/Data/Printer/Filter/DateTime.pm
+++ lib/Data/Printer/Filter/DateTime.pm
@@ -49,6 +49,14 @@ filter 'DateTime::Tiny', sub {
     return _format( $_[0]->as_string, @_ );
 };
 
+filter 'Class::Date', sub {
+    my ($object, $p) = @_;
+
+    my $string = $object->strftime("%Y-%m-%d %H:%M:%S") .  " [" . $object->tzdst . "]";
+
+    return _format( $string, @_ );
+};
+
 filter 'Date::Calc::Object', sub {
     return _format( $_[0]->string(2), @_ );
 };
@@ -133,6 +141,8 @@ several date and time manipulation classes and displays the time
 =item * L<DateTime::Duration>
 
 =item * L<DateTime::Incomplete>
+
+=item * L<Class::Date>
 
 =item * L<Time::Piece>
 
