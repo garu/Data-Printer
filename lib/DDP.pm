e4c598c09ef5e01ae465eb5cc3883c641c15eb94
David Steinbrunner <dsteinbrunner@pobox.com>
2013-06-27 07:42:24 -0400

    typo fix

--- lib/DDP.pm
+++ lib/DDP.pm
@@ -23,7 +23,7 @@ DDP - Data::Printer shortcut for faster debugging
 Tired of typing C<use Data::Printer> every time? C<DDP> lets you quickly call
 your favorite variable dumper!
 
-It behaves exacly like L<Data::Printer> - it is, indeed, just an alias to it :)
+It behaves exactly like L<Data::Printer> - it is, indeed, just an alias to it :)
 
 Happy debugging!
 
